7c34db3a869a045d2ba66509851f56e86111ca23
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-06 15:58:15 +0200

    Utworzenie komponentu Address

---
+++ src/BlazorWebAssemblyApp/BlazorWebAssemblyStandaloneApp/Models/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWebAssemblyStandaloneApp.Models
+{
+    public class Address
+    {
+        public required string City { get; set; }
+        public string? Street { get; set; }
+    }
+}
