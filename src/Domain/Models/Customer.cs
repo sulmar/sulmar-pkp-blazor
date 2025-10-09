e614207378cd19d701b752c3a961c4524a537f75
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-09 14:21:49 +0200

    Dodanie walidacji za pomoca DataAnnotations

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -1,12 +1,27 @@
-﻿namespace Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Domain.Models;
 
 public class Customer
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(10), MinLength(3)]
     public string Name { get; set; }
 
     public Address WorkAddress { get; set; }
 
+    [EmailAddress]
     public string Email { get; set; }
+
+
+    public string Code { get; set; }
+
+
+    public string Password { get; set; }
+    
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
 }
