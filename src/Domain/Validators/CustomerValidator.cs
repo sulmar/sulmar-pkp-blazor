e3b555909851f05fe97be8062164c49bc7214c18
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-09 14:53:04 +0200

    Dodanie walidacji za pomoca FluentValidations

---
+++ src/Domain/Validators/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validators;
+
+// https://docs.fluentvalidation.net/en/latest/
+// dotnet add package FluentValidation (nuget)
+public class CustomerValidator : AbstractValidator<Customer>
+{
+    public CustomerValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().Length(3, 10);
+        RuleFor(p => p.Email).EmailAddress();
+    }
+}
