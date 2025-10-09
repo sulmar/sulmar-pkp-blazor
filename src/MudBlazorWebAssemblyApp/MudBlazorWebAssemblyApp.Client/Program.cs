750339640ee992e2c2e938491dfb56a34848dac6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-09 11:04:36 +0200

    Refactor: Dodanie uslugi CustomerService

--- src/MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp.Client/Program.cs
+++ src/MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using MudBlazorWebAssemblyApp.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -7,6 +8,8 @@ builder.Services.AddMudServices();
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7002") });
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 
 await builder.Build().RunAsync();
---
+++ src/MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp.Client/Services/ICustomerService.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace MudBlazorWebAssemblyApp.Client.Services;
+
+public interface ICustomerService
+{
+    Task<List<Customer>?> GetAllAsync();
+}
+
+public class CustomerService : ICustomerService
+{
+    private HttpClient _http;
+
+    public CustomerService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public Task<List<Customer>?> GetAllAsync()
+    {
+        return _http.GetFromJsonAsync<List<Customer>>("api/customers");
+    }
+}
