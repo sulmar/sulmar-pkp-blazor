5b839b21ba30b0ad5cae0acdcfae945d1511ad43
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-17 12:02:08 +0100

    Dodanie T4 i partial class

---
+++ src/Sakila.Infrastructure/SakilaContext.Custom.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakila.Infrastructure;
+
+public partial class SakilaContext
+{
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Customer>()
+            .Property(p => p.FirstName)
+            .HasColumnType("varchar(45)");
+    }
+}
